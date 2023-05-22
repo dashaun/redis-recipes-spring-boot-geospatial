0975b4cc742829c84639fe86da7187d193ea5a59
dashaun <dashaun@dashaun.com>
2023-05-22 12:29:36 -0500

    initializing

---
+++ src/test/java/dev/dashaun/function/redis/geospatial/GeospatialApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dashaun.function.redis.geospatial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeospatialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
